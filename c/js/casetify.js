8fc9c9d1066a1dc444dae81ffdf7c746024e6919
g-y-gif <114711742+g-y-gif@users.noreply.github.com>
2022-10-06 06:48:26 +0000

    수정

--- c/js/casetify.js
+++ c/js/casetify.js
@@ -1,8 +1,9 @@
 
 
+// sec02 trending case 이미지 전환
 $(function(){
-    $("#case > div:first").show();
-    $("#tabPreview > div").click(function(){
+    $("#case > div:last").show();
+    $("#tabPreview .circle").click(function(){
         $(this).addClass("on").siblings().removeClass("on");
         var imgindex = $(this).index();
         $("#case > div").eq(imgindex).stop(true).fadeIn(500).siblings().fadeOut(200);
