6d186ee775c469ff9d78288336667c53ebd807a1
g-y-gif <114711742+g-y-gif@users.noreply.github.com>
2022-10-06 05:07:27 +0000

    수정

--- c/js/casetify.js
+++ c/js/casetify.js
@@ -1,8 +1,10 @@
 
 
 $(function(){
-    $(".tabPreview .c00").click(function(){
-        $(".case").show(".i01");
-        $(this).toggleClass("on");
+    $("#case > div:first").show();
+    $("#tabPreview > div").click(function(){
+        $(this).addClass("on").siblings().removeClass("on");
+        var imgindex = $(this).index();
+        $("#case > div").eq(imgindex).stop(true).fadeIn(500).siblings().fadeOut(200);
     });
 });
