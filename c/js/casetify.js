97c220b334848709cbff044d82494a5e6078c96c
g-y-gif <114711742+g-y-gif@users.noreply.github.com>
2022-10-14 08:29:43 +0000

    수정

--- c/js/casetify.js
+++ c/js/casetify.js
@@ -1,4 +1,15 @@
 
+// 메뉴 클릭 메서드
+$(function(){
+    $("header .menu").click(function(){
+        $("#side_nav").addClass(".is-open")
+    });
+    $(".menuclose > img").click(function(){
+        $("#side_nav").removeClass(".is-open")
+    });
+});
+
+
 
 // sec02 trending case 이미지 전환
 $(function(){
@@ -9,3 +20,4 @@ $(function(){
         $("#case > div").eq(imgindex).stop(true).fadeIn(500).siblings().fadeOut(200);
     });
 });
+
